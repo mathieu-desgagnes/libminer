bff0cfdc1fece112425a56f07ba89262a9122ec2
mathieu-desgagnes <45071036+mathieu-desgagnes@users.noreply.github.com>
2024-11-05 12:15:29 -0500

    add testthat and initial tests

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,16 @@
+test_that("lib_summary returns exected results", {
+  result <- lib_summary()
+
+  expect_s3_class(result, 'data.frame')
+
+  expect_equal(ncol(result), 2)
+  expect_gte(nrow(result),1)
+  expect_equal(names(result), c('Library','n_packages'))
+  expect_type(result$n_packages, 'integer')
+  expect_type(result$Library, 'character')
+})
+
+test_that('lib_summary fails appropiately', {
+  expect_error(lib_summary('hello'),'unused argument')
+})
+
